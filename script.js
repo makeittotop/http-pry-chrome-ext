0a4ecfce5dc4cbabc74e0f0c71045600bac2cc17
Abhishek Pareek <makeittotop@gmail.com>
2015-03-29 22:47:02 +0400

    Intercept and display HTTP requests for images across the browser

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-var get_currency_rate = function () {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "http://www.xe.com/currencyconverter/convert/?Amount=1&From=AED&To=INR", true);
+var request_objs = [];
 
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      var el = document.createElement( 'div' );
-      el.innerHTML = xhr.responseText;
+var filters = { urls: ["<all_urls>"], types: ["image"] };
 
-      var currency_text;
-      $('.uccRes .rightCol', el).each(function() { currency_text = $(this).html();});
-
-      var re = /\d+.\d+/;
-      var current_rate = currency_text.match(re)[0];
-      var aed_amount = $("#aed_amount").val()
-      $("#inr_amount").val(aed_amount * current_rate);
-    }
-  }
-
-  xhr.send();
-};
+var callback = function (info) {
+  console.log("Intercepted: " + info.url);
+  request_objs.push(info);   
+}
 
 $(document).ready(function() {
   console.log("HTTP Pry ...");
+
+  chrome.webRequest.onBeforeRequest.addListener(callback, filters);
+
 });
