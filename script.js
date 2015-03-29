374d8806a91f83b5228236e56168d71790488f81
Abhishek Pareek <makeittotop@gmail.com>
2015-03-29 23:11:25 +0400

    Block images across the browser by cancelling their originating requests

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ var request_objs = [];
 
 var filters = { urls: ["<all_urls>"], types: ["image"] };
 
+var extra_info_spec = ["blocking"];
+
 var callback = function (info) {
   console.log("Intercepted: " + info.url);
   request_objs.push(info);   
+
+  return {cancel: true};
 }
 
 $(document).ready(function() {
   console.log("HTTP Pry ...");
 
-  chrome.webRequest.onBeforeRequest.addListener(callback, filters);
+  chrome.webRequest.onBeforeRequest.addListener(callback, filters, extra_info_spec);
 
 });
