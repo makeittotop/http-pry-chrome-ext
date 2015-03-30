d1da8e18fc30041681499afaa32754966e355127
Abhishek Pareek <makeittotop@gmail.com>
2015-03-30 14:31:09 +0400

    Block all originating ajax requests on a particular web domain. Cruel

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var request_objs = [];
 
-var filters = { urls: ["<all_urls>"], types: ["image"] };
+//var filters = { urls: ["<all_urls>"], types: ["image"] };
+var filters = { urls: ["*://drive.google.com/*"], types: ["xmlhttprequest"] };
 
 var extra_info_spec = ["blocking"];
 
@@ -8,9 +9,9 @@ var callback = function (info) {
   console.log("Intercepted: " + info.url);
   request_objs.push(info);   
 
-  //return { cancel: true };
+  return { cancel: true };
   // redirects all images to some cute cat image 
-  return { redirectUrl: "http://susanspetblog.com/wp-content/uploads/2014/12/Love-Cute-cat-kitty.jpg" };
+  // return { redirectUrl: "http://susanspetblog.com/wp-content/uploads/2014/12/Love-Cute-cat-kitty.jpg" };
 }
 
 $(document).ready(function() {
